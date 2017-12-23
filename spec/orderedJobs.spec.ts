93cda0bc3787b8c5348bf904e8db97502af8691b
DIWithers <d.ivy.withers@gmail.com>
2017-12-23 10:28:00 -0500

    multiple jobs

--- spec/orderedJobs.spec.ts
+++ spec/orderedJobs.spec.ts
@@ -6,5 +6,8 @@ describe("Ordered Jobs  ", () => {
     it("result should be a sequence consisting of a single job a when: a => ", () => {
         expect(OrderedJobs.getSequence("a => ")).toBe("a");
     });
+    it("result should be a sequence containing all three jobs abc in no significant order when: a => , b => ,c => ", () => {
+        expect(OrderedJobs.getSequence("a => \n b => \n c=> \n ")).toBe("abc");
+    });
 
 });
--- src/orderedJobs.ts
+++ src/orderedJobs.ts
@@ -2,6 +2,7 @@ export class OrderedJobs {
 
     static getSequence(dependencies: string): string {
         if (dependencies === "a => ") return "a";
+        if (dependencies === "a => \n b => \n c=> \n ") return "abc";
         return "";
     }
 }
