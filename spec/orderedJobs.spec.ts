69b60f271ac69fad3354d1ea0e003173007ed649
DIWithers <d.ivy.withers@gmail.com>
2017-12-27 17:59:35 -0500

    multiple jobs, single dependency

--- spec/orderedJobs.spec.ts
+++ spec/orderedJobs.spec.ts
@@ -3,7 +3,7 @@ describe("Ordered Jobs  ", () => {
     it("result should be an empty sequence when passed an empty string (no jobs)", () => {
         expect(OrderedJobs.getSequence("")).toBe("");
     });
-    it("result should be a sequence consisting of a single job a when: a => ", () => {
+    it("result should be a sequence consisting of a single name a when: a => ", () => {
         expect(OrderedJobs.getSequence("a => ")).toBe("a");
     });
     it("result should be a sequence containing all three jobs abc in no significant order when: a => , b => ,c => ", () => {
--- src/orderedJobs.ts
+++ src/orderedJobs.ts
@@ -1,14 +1,37 @@
 export class OrderedJobs {
 
     static getSequence(jobs: string): string {
+        let jobsSequence: any = this.getJobSequence(jobs);
+        let sequence: any = "";
+        // console.log(jobsSequence);
+        // for (let name of Array.from(jobsSequence.entries())) {
+        //     // console.log(name);
+        // }
+        jobsSequence.forEach((job: any) => {
+            console.log(job);
+            if (job.dependency === undefined && sequence.indexOf(job.name) === -1) sequence += job.name;
+
+            if (job.dependency !== undefined) {
+                sequence += job.dependency;
+                sequence += job.name;
+            }
+            //
+            // // if (job.dependency !== undefined) sequence.slice(0, 1) + job.name + sequence.slice(1);
+
+        });
+        if (jobs !== "") return sequence;
+        // console.log(sequence);
+        // if (jobs === "a => ") return "a";
+        // if (jobs === "a => \n b => \n c => ") return "abc";
+        return "";
+    }
+
+    private static getJobSequence(jobs: string): any {
         let jobsSequence: any = jobs.split("\n");
         jobsSequence = jobsSequence.map((job: any) => {
             let newJob: any = job.replace(/\W+/g, "");
-            return {job: newJob[0], dependency: newJob[1]};
+            return {name: newJob[0], dependency: newJob[1]};
         })
-
-        if (jobs === "a => ") return "a";
-        if (jobs === "a => \n b => \n c => ") return "abc";
-        return "";
+        return jobsSequence;
     }
 }
