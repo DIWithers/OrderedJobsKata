c9859a91182679523fe6ebc1e665617cce9b6859
DIWithers <d.ivy.withers@gmail.com>
2017-12-29 00:56:47 -0500

    multi jobs, self referencing dependency

--- spec/orderedJobs.spec.ts
+++ spec/orderedJobs.spec.ts
@@ -15,5 +15,8 @@ describe("Ordered Jobs  ", () => {
     it("result should be a sequence that positions f before c, c before b, b before e and a before d, when: a => , b => c, c => f, d => a, e => b, f => ", () => {
         expect(OrderedJobs.getSequence("a => \n b => c \n c => f \n d => a \n e => b \n f => ")).toBe("adfcbe");
     });
+    it("result should be an error stating that jobs can’t depend on themselves", () => {
+        expect(OrderedJobs.getSequence("a => \n b => c \n c => c")).toBe("Error: Jobs cannot depend on themselves.");
+    });
 
 });
--- src/orderedJobs.ts
+++ src/orderedJobs.ts
@@ -3,17 +3,30 @@ export class OrderedJobs {
     static getSequence(jobs: string): string {
         let jobsDependencies: any = this.getJobDependencies(jobs);
         let sequence: any = [];
+        let invalidDependency: boolean = this.checkForInvalidDependency(jobsDependencies);
+        if (jobs === "") return "";
+        if (invalidDependency) return "Error: Jobs cannot depend on themselves.";
         this.addJobsWithNoDependencies(jobsDependencies, sequence);
+        this.addJobsWithDependencies(jobsDependencies, sequence);
+        return sequence.toString().replace(/,/g, "");
+    }
+
+    private static checkForInvalidDependency(jobsDependencies: any): boolean {
+        let invalidDependency: boolean = false;
+        jobsDependencies.forEach((job: any) => {
+            if (job.name === job.dependency) invalidDependency = true;
+        });
+        return invalidDependency;
+    }
+
+    private static addJobsWithDependencies(jobsDependencies: any, sequence: any): any {
         jobsDependencies.forEach((job: any) => {
             if (job.dependency !== undefined) {
                 if (sequence.indexOf(job.dependency) === -1) sequence.push(job.dependency);
                 let whereToInsert: number = sequence.indexOf(job.dependency) + 1;
-                if (sequence.indexOf(job.name) === -1)sequence.splice(whereToInsert, 0, job.name);
+                if (sequence.indexOf(job.name) === -1) sequence.splice(whereToInsert, 0, job.name);
             }
         });
-        if (jobs !== "") return sequence.toString().replace(/,/g, "");
-
-        return "";
     }
 
     private static addJobsWithNoDependencies(jobsDependencies: any, sequence: any): any {
